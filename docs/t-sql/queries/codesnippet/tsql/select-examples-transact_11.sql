9afbd965f47cd18cb565106921261b1fc99ae6cb
WilliamAntonRohm <v-wiroh@microsoft.com>
2019-11-05 09:47:11 -0800

    addressing sql-docs issue 3455

--- docs/t-sql/queries/codesnippet/tsql/select-examples-transact_11.sql
+++ docs/t-sql/queries/codesnippet/tsql/select-examples-transact_11.sql
@@ -4,7 +4,7 @@ SELECT p1.ProductModelID
 FROM Production.Product AS p1
 GROUP BY p1.ProductModelID
 HAVING MAX(p1.ListPrice) >= ALL
-    (SELECT AVG(p2.ListPrice)
+    (SELECT AVG(p2.ListPrice) * 2
      FROM Production.Product AS p2
      WHERE p1.ProductModelID = p2.ProductModelID);
 GO
